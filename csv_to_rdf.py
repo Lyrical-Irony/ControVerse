fa352af80da850912916ac2fd99a693fb759d795
ValkyrieCain9 <kemymanyara@gmail.com>
2025-11-05 15:04:35 +0100

    reviewing the python

--- csv_to_rdf.py
+++ csv_to_rdf.py
@@ -3,12 +3,14 @@ from rdflib import Graph, Namespace, Literal, RDF, URIRef
 from rdflib.namespace import XSD
 
 COVER = Namespace("https://w3id.org/controverse#")
+COVERCORE = Namespace("https://w3id.org/controverse/")
 
 # Graph
 g = Graph()
 g.parse("C:\\Users\\ilari\\Desktop\\PYTHON\\GANGEMI\\CoVer_Ontology.ttl", format="turtle")
 
 g.bind("cover", COVER)
+g.bind("covercore", COVERCORE)
 
 # handling Literals and Object properties 
 def as_node(value: str):
@@ -29,11 +31,17 @@ def as_node(value: str):
 with open("C:\\Users\\ilari\\Desktop\\PYTHON\\GANGEMI\\creative_works.csv", newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=',')
     for row in reader:
-        work_id = row["ID"].strip()
-        work_uri = COVER[work_id] 
+        inst_uri = COVERCORE[row["ID"].strip()]
+        event_uri = COVERCORE["event"+row["ID"]] 
+        work_uri = COVERCORE[row["Creative Work"].strip()]
 
-        
-        g.add((work_uri, RDF.type, COVER.CreativeWork))
+        if row["Instance Type"] == "Irony":
+            g.add((inst_uri, RDF.type, COVER.Irony))
+            g.add((event_uri, RDF.type, COVER.IronicEvent))
+        else:
+            g.add((inst_uri, RDF.type, COVER.Sincerity))
+            g.add((event_uri, RDF.type, COVER.SincereEvent))
+
 
         mapping = {
             "Creative Work" : COVER.hasTitle,
@@ -59,6 +67,10 @@ with open("C:\\Users\\ilari\\Desktop\\PYTHON\\GANGEMI\\creative_works.csv", newl
         }
 
         # triples
+        #triples for instance
+        #triple for event
+        #triple for work
+
         for col, prop in mapping.items(): 
             val = row.get(col, "").strip()
             if not val:
